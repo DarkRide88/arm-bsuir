791b8686b35f8bc08fe54fa2c9d046ae541dbe6f
DarkRide88 <dark88ride@mail.ru>
2020-11-02 15:18:39 +0300

    fix: fix Input styles

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -9,20 +9,22 @@ const Input = props => {
   const inputType = props.type || 'text'
   const classes = [styles.input]
   const htmlFor = `${inputType}-${Math.random()}`
-
+  classes.push(props.className)
   if (isInvalid(props)) {
-    classes.push(styles.invalid)
+    classes.push(styles.invalid)  
   }
   return (
-    <div className={classes.join(' ')}>
-      <label htmlFor={htmlFor}>{props.label}</label>
+    <div className={classes.join(' ') }>
+      <label htmlFor={htmlFor} >{props.label}</label>
       <input
+        
         maxLength={props.maxlength}
         type={inputType}
         id={htmlFor}
         value={props.value}
         onChange={props.onChange}
         placeholder={props.placeholder}
+   
        
       />
 
